8d217f4b7a95ce8c8ad2d566a751d9610a197a37
McZlatan Bourne <bournethesonofthegun@gmail.com>
2017-10-10 17:56:52 +0100

    Wrote tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,7 +2,22 @@ var expect = require('chai').expect;
 var yorubaNames = require('../src/index');
 
 describe('yoruba-names', function() {
-    it('o yẹ ki o ṣiṣẹ!', function() {
-        expect(true).to.be.true;
+    describe('all', function() {
+        it('should return an array of names', function() {
+            expect(yorubaNames.all).to.satisfy(isArrayOfStrings);
+
+            function isArrayOfStrings(array) {
+                return array.every(function(item) {
+                    return typeof item === 'string';
+                });
+            }
+        });
+    });
+
+    describe('random', function() {
+        it('should return a random name', function() {
+            var randomName = yorubaNames.random();
+            expect(yorubaNames.all).to.include(randomName);
+        });
     });
 });
