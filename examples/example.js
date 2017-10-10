e4809a4e235b273daa65bc26a80c7a9922c76dd2
McZlatan Bourne <bournethesonofthegun@gmail.com>
2017-10-10 16:36:05 +0100

    Added Readme, Created and example file, Added more yoruba names, bumped up version tag

---
+++ examples/example.js
@@ -0,0 +1,9 @@
+var yorubaNames = require('yoruba-names');
+
+// Get a random Yoruba name
+console.log(yorubaNames.random());
+console.log(yorubaNames.random());
+
+// Get a random Yoruba fullname
+console.log(yorubaNames.random() + " " + yorubaNames.random());
+console.log(yorubaNames.random() + " " + yorubaNames.random());
