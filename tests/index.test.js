ad5ae95b1b2acb38bdd53e3c0749621153a4337d
McZlatan Bourne <bournethesonofthegun@gmail.com>
2017-10-10 17:36:27 +0100

    Added Mocha and Chai and wrote a simple test

---
+++ tests/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var yorubaNames = require('../src/index');
+
+describe('yoruba-names', function() {
+    it('o yẹ ki o ṣiṣẹ!', function() {
+        expect(true).to.be.true;
+    });
+});
